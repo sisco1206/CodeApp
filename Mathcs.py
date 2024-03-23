02c43741836a2a74e44c674003fc7915cde5baa8
sisco1206 <97121785+sisco1206@users.noreply.github.com>
2024-03-22 19:47:56 -0500

    Update Mathcs.py file

--- Mathcs.py
+++ Mathcs.py
@@ -1,1 +1,11 @@
-print("The beach is close today, and I was ready to have a good time" 
+pip install flask
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+
+def home():
+  return "Good morning Francis"
+if __name__ == "__main__":
+    app.run(debug=True)
+
